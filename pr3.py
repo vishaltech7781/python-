8f2b50e40ca319f508214ca982d12235aa46ba60
vishaltech7781 <vishalbiradar21412@gmail.com>
2025-02-05 22:42:59 +0530

    chapter 2 problems

---
+++ pr3.py
@@ -0,0 +1,16 @@
+a = input(" enetr the number 1 : ")
+
+b = input(" enter the number 2 : ")
+
+print("1st number entered is : ",a)
+print("2nd nummber entered is : ",b)
+print("sum of number is : ",a+b)
+
+
+a = int(input(" enetr the number 1 : "))
+
+b = int (input(" enter the number 2 : "))
+
+print("1st number entered is : ",a)
+print("2nd nummber entered is : ",b)
+print("sum of number is : ",a+b)
---
+++ pr4.py
@@ -0,0 +1,11 @@
+a = 34
+
+b = 80
+
+print (" check weather a is greater than b or not :", a>b)
+
+#alternative way to write by taking values from user 
+a = int(input( "enter the 1st no. :"))
+b = int(input( " enter the 2nd no. :"))
+
+print ( "check weather a is greater than b or not ", a>b)

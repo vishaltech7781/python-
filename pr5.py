7f16108b81d1054a7b5bf3df74ac2443fd96db5a
vishaltech7781 <vishalbiradar21412@gmail.com>
2025-02-06 19:17:12 +0530

    chapter 2 problems

---
+++ pr5.py
@@ -0,0 +1,11 @@
+a = 34
+
+b = 80
+
+print (" the average of the two numbers is:", (a+b)/2)
+
+#alternative way to write by taking values from user 
+a = int(input( "enter the 1st no. :"))
+b = int(input( " enter the 2nd no. :"))
+
+print ( " average of the two numbers entered is ", (a+b)/2)               
---
+++ pr6.py
@@ -0,0 +1,5 @@
+a = int (input( " entered number is :"))
+
+print( " square of the number entered is :",a*a)
+#we cant use '^' this symbol for taking square of any root 
+print ("4th root of the entered nummber is :",a**4)

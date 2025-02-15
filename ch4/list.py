208d60267d5a7f7241628594ae7d75df5111ee83
vishal_7781 <vishalbiradar21412@gmail.com>
2025-02-15 11:42:07 +0530

    Add files via upload

---
+++ ch4/list.py
@@ -0,0 +1,18 @@
+friend = [ 7,9,False,"harry","vishal"]
+print(friend)
+print(friend[0])
+print(friend[0:4])
+print(type(friend))
+
+#list methods
+l1=[1,3,2,56,34,44]
+l1.sort()
+print(l1)
+l1.append(7)
+print(l1)
+l1.insert(3,7)
+print(l1)
+l1.pop(4)
+print(l1)
+l1.remove(44)
+print(l1)
---
+++ ch4/tuple.py
@@ -0,0 +1,38 @@
+a= (1,2 ,4,4534,34,44,52,2,4,4534)
+a.index(4534)
+#a.index(3)
+a.count(4534)
+a.count(3434)
+
+#concantetination
+tuple1=(1,2,3)
+tuple2=(3,4,50)
+print(tuple1+tuple2)
+
+#repetetion
+t1=(1,2,3)
+print(t1*3)
+
+#membership
+t1=(1,2,4)
+print(2 in t1)
+print(5 in t1)
+
+#length
+t1=(1,2,3,4)
+print(len(t1))
+
+#min&max
+t1=(1,2,3,4,5,6,7)
+print(min(t1))
+print(max(t1))
+
+#slicing
+t1=(1,2,3,4,5)
+print(t1[1:4])
+
+#unpacking
+t1=(1,2,3)
+a,b,c = t1
+print(a,b,c)
+

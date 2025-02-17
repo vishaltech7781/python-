e9ebf7e4263758963c25af32803e49d78144753e
vishal_7781 <vishalbiradar21412@gmail.com>
2025-02-18 00:11:54 +0530

    Add files via upload

---
+++ ch4/pr2.py
@@ -0,0 +1,18 @@
+marks = []
+
+f1 = int (input("enter marks here of students : "))
+marks.append(f1)
+f2 = int (input("enter marks here of students : "))
+marks.append(f2)
+f3 = int (input("enter marks here of students : "))
+marks.append(f3)
+f4 = int (input("enter marks here of students : "))
+marks.append(f4)
+f5 = int (input("enter marks here of students : "))
+marks.append(f5)
+f6 = int (input("enter marks here of students : "))
+marks.append(f6)
+
+marks.sort()
+
+print(marks)
---
+++ ch4/prblm_1.py
@@ -0,0 +1,17 @@
+fruits = []
+
+f1 = input("enter the name of fruit to be added: ")
+fruits.append(f1)
+f2 = input("enter the name of fruit to be added: ")
+fruits.append(f2)
+f3 = input("enter the name of fruit to be added: ")
+fruits.append(f3)
+f4 = input("enter the name of fruit to be added: ")
+fruits.append(f4)
+f5 = input("enter the name of fruit to be added: ")
+fruits.append(f5)
+f6 = input("enter the name of fruit to be added: ")
+fruits.append(f6)
+f7 = input("enter the name of fruit to be added: ")
+fruits.append(f7)
+print(fruits)

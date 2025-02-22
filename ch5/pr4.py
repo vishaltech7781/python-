f33c50e9ddf116020b6970d030937d8070fde1ab
vishal_7781 <vishalbiradar21412@gmail.com>
2025-02-22 11:42:18 +0530

    Add files via upload

---
+++ ch5/pr4.py
@@ -0,0 +1,5 @@
+s ={}
+#what is the type of 's' 
+print(type(s))  #we use in dict dict_name = {} [for empty set]
+                #but for sert we use set_name = set() [for empty set creation]
+                
---
+++ ch5/pr5.py
@@ -0,0 +1,22 @@
+d = {}
+
+name = input("enter the name of friends : ")
+lang = input("enter the language u like :")
+
+d.update({name:lang})
+name = input("enter the name of friends : ")
+lang = input("enter the language u like :")
+
+d.update({name:lang})
+name = input("enter the name of friends : ")
+lang = input("enter the language u like :")
+
+d.update({name:lang})
+name = input("enter the name of friends : ")
+lang = input("enter the language u like :")
+
+d.update({name:lang})
+
+print(d)
+print(d.values())
+print(d.keys())

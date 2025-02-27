e07c5c2db5872d92bdf7385c8f59e9be71193c6d
vishal_7781 <vishalbiradar21412@gmail.com>
2025-02-27 20:52:01 +0530

    Add files via upload

---
+++ ch6/pr3.py
@@ -0,0 +1,11 @@
+list = [ "harry","vishal ","om", "atharva","vinay"]
+
+name = input ("enter the name to be checked in the list :")
+
+if ( name in list ):{
+    print ("then the namen is the list ")
+}
+    
+else :{
+    print (" entered name is not in the list")
+}    
---
+++ ch6/pr4.py
@@ -0,0 +1,13 @@
+c1 = "make a lot of money "
+c2 = " buy now "
+c3 = "subscribe this "
+c4 =" click this "
+
+comment = input("enter the comment :")
+
+if( c1 in comment or c2 in comment  or c3 in comment or c4 in comment ):{
+    print ("then this comment is a spam avoid this mail u can be scammed ")  
+}
+else : {
+        print ("it is not spam mail ")
+    }

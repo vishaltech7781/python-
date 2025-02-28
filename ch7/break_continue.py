a575adf37709de49f416082273f01a33783b6835
vishal_7781 <vishalbiradar21412@gmail.com>
2025-02-28 19:08:54 +0530

    Add files via upload

---
+++ ch7/break_continue.py
@@ -0,0 +1,19 @@
+# for i in range (0,100):
+#     if(i==33):  #it well skip further printing of numbers and exits from for loop
+#         break;
+#     print(i)
+
+  
+# for i in range (0,100):
+#     if(i==33):  #it will skip the iteration and further proceed printing
+#         continue
+#     print(i)
+
+
+for i in range (0,100):
+    pass 
+
+i=0
+while(i<30):
+    print(i)
+    i = i+1

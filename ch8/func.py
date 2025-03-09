84214e0936f4b99083ca884032be1a30a629a2f2
vishal_7781 <vishalbiradar21412@gmail.com>
2025-03-09 15:36:36 +0530

    Add files via upload

---
+++ ch8/func.py
@@ -0,0 +1,14 @@
+
+def avg():  # function defination 
+                  
+    a = int(input("enter the numberr :"))       
+    b = int(input("enter the numberr :"))       
+    c = int(input("enter the numberr :"))       
+    
+    average = (a + b +c )/3
+    print( "average of the number is :",average)
+
+avg()   #function calling
+avg()
+avg()
+avg()
